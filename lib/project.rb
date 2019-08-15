334154fc915e895812d28c3ecaa58fd13b837d5c
LaNice Powell <lanicemp@aol.com>
2019-08-15 10:52:36 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project  
+  
+  attr_reader :title, :backers
+  #attr_accessor :backed_projects
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+  end 
+end 
